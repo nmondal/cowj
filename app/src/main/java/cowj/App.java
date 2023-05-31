747fca9ed4daa42fcf197bcc84b267b22e1fd8ab
Nabarun Mondal <nabarun.mondal@gmail.com>
2023-05-31 11:23:09 +0530

    Updated with basic model parsing.

--- app/src/main/java/cowj/App.java
+++ app/src/main/java/cowj/App.java
@@ -4,11 +4,6 @@
 package cowj;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
     }
 }
---
+++ app/src/main/java/cowj/Model.java
@@ -0,0 +1,79 @@
+package cowj;
+
+import zoomba.lang.core.types.ZTypes;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+public interface Model {
+
+    String base();
+
+    default int port(){
+        return 8080;
+    }
+
+    default String staticPath(){
+        return "static" ;
+    }
+
+    default Map<String, Map<String,String>> routes(){
+        return Collections.emptyMap();
+    }
+
+    default Map<String,String> auth(){
+        return Collections.emptyMap();
+    }
+
+    String PORT = "port" ;
+    String STATIC = "static" ;
+    String ROUTES = "routes" ;
+    String AUTH = "routes" ;
+
+    static Model from(final Map<String,Object> map, final String baseDir){
+
+        return new Model() {
+
+            @Override
+            public String base(){ return baseDir; }
+            @Override
+            public int port() {
+                return (int)map.getOrDefault(PORT, Model.super.port());
+            }
+
+            @Override
+            public String staticPath() {
+                return (String) map.getOrDefault(STATIC, Model.super.staticPath());
+            }
+
+            @Override
+            public Map<String, Map<String, String>> routes() {
+                return (Map) map.getOrDefault( ROUTES, Model.super.routes());
+            }
+
+            @Override
+            public Map<String, String> auth() {
+                return (Map) map.getOrDefault( ROUTES, Model.super.auth());
+            }
+        };
+    }
+
+    static Model from(String path){
+        File f = new File(path);
+        final String abs = f.getAbsolutePath();
+        if ( !f.exists() ) throw new RuntimeException(String.format("No file name : '%s' ", abs ));
+        final String lc = abs.toLowerCase(Locale.ROOT);
+        final String baseDir = f.getAbsoluteFile().getParent();
+        if ( lc.endsWith(".yaml") || lc.endsWith(".yml")){ //  yaml load
+            Map m = (Map)ZTypes.yaml(abs,true);
+            return from(m,baseDir);
+        }
+        if ( lc.endsWith(".json") ){ //  json load
+            Map m = (Map)ZTypes.json(abs,true);
+            return from(m,baseDir);
+        }
+        throw new RuntimeException(String.format("file '%s' must be a json or yaml", abs ));
+    }
+}
