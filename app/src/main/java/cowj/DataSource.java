4eccc1b4e7663505a98a998ecc57ce168a9c9860
Nabarun Mondal <nabarun.mondal@gmail.com>
2023-06-24 13:19:52 +0530

    Updated with Plugin Error tests.

--- app/src/main/java/cowj/DataSource.java
+++ app/src/main/java/cowj/DataSource.java
@@ -15,27 +15,28 @@ public interface DataSource {
 
     Map<String,Creator> REGISTRY = new HashMap<>();
 
-    static void registerType(String type, String path){
+    static EitherMonad<Creator> registerType(String type, String path){
         String[] paths = path.split("::");
         try {
             Class<?> clazz = Class.forName(paths[0]);
             Field f = clazz.getDeclaredField(paths[1]);
             Object r = f.get(null);
             if ( !(r instanceof Creator) ){
-                System.err.println( "Error registering type... not a creator object : " + r.getClass());
-                return;
+                throw new IllegalArgumentException("Error registering type... not a creator object : " + r.getClass());
             }
             REGISTRY.put(type,(Creator) r);
             System.out.printf("Registered '%s::%s' as provider type for '%s' %n", clazz.getName(), f.getName(), type );
+            return EitherMonad.value((Creator) r);
         }catch (Throwable t){
             System.err.println( "Error registering type... : " + t);
+            return EitherMonad.error(t);
         }
     }
 
     Creator UNIVERSAL = (name, config, parent) -> {
         String type = config.getOrDefault("type", "").toString();
         Creator creator = REGISTRY.get(type);
-        if (creator == null) throw new IllegalStateException("Unknown type of datasource -> value: " + type);
+        if (creator == null) throw new IllegalArgumentException("Unknown type of datasource -> value: " + type);
         return creator.create(name, config, parent);
     };
 }
---
+++ app/src/test/java/cowj/PluginsTest.java
@@ -0,0 +1,35 @@
+package cowj;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertThrows;
+
+public class PluginsTest {
+
+    @Test
+    public void testNonExistingClass(){
+        EitherMonad<DataSource.Creator> em = DataSource.registerType("foo", "a.b.c::FOO");
+        Assert.assertTrue(em.inError());
+        Assert.assertTrue( em.error().getMessage().contains("a.b.c"));
+    }
+
+    @Test
+    public void testExistingButInvalidClass(){
+        EitherMonad<DataSource.Creator> em = DataSource.registerType("foo", "java.lang.String::CASE_INSENSITIVE_ORDER");
+        Assert.assertTrue(em.inError());
+        Assert.assertTrue( em.error().getMessage().contains("not a creator"));
+    }
+
+    @Test
+    public void unRegisteredTypeTest(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            DataSource.UNIVERSAL.create("foo", Map.of("type", "bar"), null);
+        });
+        Assert.assertNotNull(exception);
+        Assert.assertTrue( exception.getMessage().contains("Unknown type of datasource"));
+        Assert.assertTrue( exception.getMessage().contains("bar"));
+    }
+}
