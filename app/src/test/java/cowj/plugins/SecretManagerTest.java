76b8ab35e12cfbb907bcd9366139ffe6027e942e
Nabarun Mondal <nabarun.mondal@gmail.com>
2023-06-25 19:23:58 +0530

    Updated with SecretManagerTest

--- app/src/test/java/cowj/plugins/SecretManagerTest.java
+++ app/src/test/java/cowj/plugins/SecretManagerTest.java
@@ -1,13 +1,20 @@
 package cowj.plugins;
 
+import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1.SecretPayload;
+import com.google.cloud.secretmanager.v1.SecretVersionName;
+import com.google.protobuf.ByteString;
 import cowj.DataSource;
 import cowj.Model;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
 import java.util.Collections;
 
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class SecretManagerTest {
 
@@ -25,7 +32,26 @@ public class SecretManagerTest {
 
     @Test
     public void testGSM(){
-        // TODO get this thing setup
+        MockedStatic<SecretManagerServiceClient> smscStatic =  mockStatic(SecretManagerServiceClient.class);
+        SecretManagerServiceClient smsc = mock( SecretManagerServiceClient.class);
+        smscStatic.when( SecretManagerServiceClient::create).thenReturn(smsc);
+        AccessSecretVersionResponse resp = mock(AccessSecretVersionResponse.class);
+        SecretPayload sp = mock(SecretPayload.class);
+        ByteString bs = ByteString.copyFromUtf8( "{ \"a\" : \"42\" }");
+        when(sp.getData()).thenReturn(bs);
+        when(resp.getPayload()).thenReturn(sp);
+        when(smsc.accessSecretVersion((SecretVersionName) any()) ).thenReturn(resp);
+        DataSource ds =  SecretManager.GSM.create("foo", Collections.emptyMap(), model  );
+        Assert.assertNotNull(ds);
+        Assert.assertEquals("foo", ds.name() );
+        Assert.assertTrue( ds.proxy() instanceof SecretManager );
+        SecretManager sm = (SecretManager) ds.proxy();
+        String r = sm.getOrDefault("a", "0" );
+        Assert.assertEquals("42", r);
+    }
+
+    @Test
+    public void testGSMOnError(){
         Exception exception = assertThrows(Exception.class, () -> {
             DataSource ds =  SecretManager.GSM.create("bar", Collections.emptyMap(), model  );
         });
