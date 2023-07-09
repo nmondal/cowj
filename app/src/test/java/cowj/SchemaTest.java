2eec074d141960ea2529a6adccf60ecf373276a5
noga <nabarun.mondal@gmail.com>
2023-07-09 21:55:13 +0530

    Adding tests.

--- app/src/test/java/cowj/SchemaTest.java
+++ app/src/test/java/cowj/SchemaTest.java
@@ -1,9 +1,26 @@
 package cowj;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import zoomba.lang.core.types.ZTypes;
+
+import java.util.Map;
 
 public class SchemaTest {
+
+    @Before
+    public void before(){
+        mr = null;
+    }
+
+    @After
+    public void after(){
+        if ( mr == null ) return;
+        mr.stop();
+        mr = null;
+    }
 
     @Test
     public void loadSchemaTest(){
@@ -12,8 +29,31 @@ public class SchemaTest {
         Assert.assertEquals( 2, typeSystem.routes().size());
     }
 
-    @Test
-    public void liveSchemaTest(){
+    ModelRunner mr;
 
+    @Test
+    public void validSchemaTest(){
+        mr = ModelRunnerTest.runModel("samples/prod/prod.yaml" );
+        String r = ModelRunnerTest.get( "http://localhost:5042", "/person/foobar");
+        Assert.assertNotNull(r);
+        Assert.assertTrue( r.contains("foobar") );
+        Assert.assertTrue( r.contains("not found") );
+        String body = ZTypes.jsonString(Map.of( "firstName", "foo", "lastName", "bar"));
+        r = ModelRunnerTest.post( "http://localhost:5042", "/person",  body);
+        Assert.assertNotNull(r);
+        Map<String,String> m = (Map)ZTypes.json(r);
+        String id = m.get("personId");
+        Assert.assertNotNull(id);
+        r = ModelRunnerTest.get( "http://localhost:5042", "/person/" + id );
+        Assert.assertNotNull(r);
+        Assert.assertTrue( r.contains(id) );
+    }
+
+    @Test
+    public void invalidSchemaTest(){
+        mr = ModelRunnerTest.runModel("samples/prod/prod.yaml" );
+        String r = ModelRunnerTest.post( "http://localhost:5042", "/person",  "foo bar!" );
+        Assert.assertNotNull(r);
+        Assert.assertTrue( r.contains("Validation") );
     }
 }
