e9aee7e96489cdc7d666011695af51e381bfed4c
noga <nabarun.mondal@gmail.com>
2023-07-29 12:04:03 +0530

    Adding more code coverage.

--- app/src/test/java/cowj/AuthenticatorTest.java
+++ app/src/test/java/cowj/AuthenticatorTest.java
@@ -10,28 +10,40 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class AuthenticatorTest {
+    static class CachedAuthTestImpl extends Authenticator.TokenAuthenticator.CachedAuthenticator{
+        int[] counters;
+        public CachedAuthTestImpl(int[] counters, int cacheSize){
+            super();
+            super.maxCapacity = cacheSize;
+            this.counters = counters;
+        }
+        @Override
+        protected UserInfo tryGetUserInfo(String token) throws Exception {
+            counters[0] ++;
+            return UserInfo.userInfo("id","bar", Long.MAX_VALUE);
+        }
+        @Override
+        public String tokenExpression() {
+            return "header:foo" ;
+        }
+    }
 
     @Test
     public void headerTokenAuthTest(){
         Request request = mock(Request.class);
         when(request.headers("foo")).thenReturn("bar");
         int[] counters = { 0 };
-        Authenticator authenticator = new Authenticator.TokenAuthenticator.CachedAuthenticator() {
-            @Override
-            protected UserInfo tryGetUserInfo(String token) throws Exception {
-                counters[0] ++;
-                return UserInfo.userInfo("id","bar", Long.MAX_VALUE);
-            }
-            @Override
-            public String tokenExpression() {
-                return "header:foo" ;
-            }
-        };
+        Authenticator authenticator = new CachedAuthTestImpl(counters,1);
         String user = authenticator.authenticate(request);
         Assert.assertEquals("id", user);
         user = authenticator.authenticate(request);
         // cache
         Assert.assertEquals(1, counters[0]);
+        // Cache test with another request...
+        Request request2 = mock(Request.class);
+        when(request2.headers("foo")).thenReturn("bar2");
+        user = authenticator.authenticate(request2);
+        Assert.assertEquals("id", user);
     }
 
     @Test
