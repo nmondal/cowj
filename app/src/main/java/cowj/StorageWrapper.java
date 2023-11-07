552dc998ea6d3f4e8d97e2037976dcdfdaceb722
noga <nabarun.mondal@gmail.com>
2023-11-07 19:41:15 +0530

    Refactoring storage abstraction from a base.

---
+++ app/src/main/java/cowj/StorageWrapper.java
@@ -0,0 +1,177 @@
+package cowj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import zoomba.lang.core.types.ZTypes;
+
+import java.util.stream.Stream;
+
+/**
+ * Wrapper to be used for underlying cloud storage
+ * @param <B> Type of response of bucket creation
+ * @param <R> Type of response of writing to a key/file
+ * @param <I> Type of item which is encountered in iteration within a bucket
+ */
+public interface  StorageWrapper <B,R,I> {
+
+    /**
+     * Logger for the wrapper
+     */
+    Logger logger = LoggerFactory.getLogger(StorageWrapper.class);
+
+
+    default boolean safeBoolean( Runnable runnable ){
+        try {
+            runnable.run();
+            return true;
+        }catch (Throwable th){
+            logger.warn("Will return false - Error happened " + th);
+            return false;
+        }
+    }
+
+    /**
+     * Dump String to Cloud Storage
+     * @param bucketName the bucket
+     * @param fileName   the file
+     * @param data       which to be dumped encoding used is UTF-8
+     * @return a R object
+     */
+    R dumps( String bucketName, String fileName, String data);
+    /**
+     * Dump Object to Storage after converting it to JSON String
+     *
+     * @param bucketName the bucket
+     * @param fileName   the file
+     * @param obj        which to be dumped
+     * @return a Blob object
+     */
+    default R dump(String bucketName, String fileName, Object obj) {
+        String objJsonString = ZTypes.jsonString(obj);
+        return dumps(bucketName, fileName, objJsonString);
+    }
+
+    /**
+     * In case file exists
+     * @param bucketName in the bucket name
+     * @param fileName having the name
+     * @return true if it is a blob , false if it does not exist
+     */
+    boolean fileExist(String bucketName, String fileName);
+
+    I data(String bucketName, String fileName);
+
+    byte[] bytes(I input);
+
+    String utf8(I input);
+
+    /**
+     * Utility method to get content of a Blob as JSON Object
+     * If not JSON object simply returns the String
+     * @param input the Blob whose content we shall read
+     * @return JSON Object if possible - else String content
+     */
+    default Object json(I input){
+        String data = utf8(input);
+        try {
+            return ZTypes.json(data);
+        } catch (Throwable t) {
+            return data;
+        }
+    }
+
+    /**
+     * Load data from Google Storage as bytes
+     *
+     * @param bucketName from this bucket name
+     * @param fileName   from this file
+     * @return byte[] - content of the file
+     */
+    default byte[] loadb(String bucketName, String fileName){
+        return bytes(data(bucketName, fileName));
+    }
+
+    /**
+     * Load data from Google Storage as String - encoding is UTF-8
+     *
+     * @param bucketName from this bucket name
+     * @param fileName   from this file
+     * @return data string - content of the file
+     */
+    default String loads(String bucketName, String fileName) {
+        return utf8( data(bucketName, fileName));
+    }
+
+    /**
+     * Load data from Google Storage as Object
+     *
+     * @param bucketName from this bucket name
+     * @param fileName   from this file
+     * @return data object - content of the file after parsing it as JSON
+     */
+    default Object load(String bucketName, String fileName) {
+        String data = loads(bucketName, fileName);
+        if (data.isEmpty()) return null;
+        try {
+            return ZTypes.json(data);
+        } catch (Throwable t) {
+            logger.warn("Error making json from binary data - will return raw data: " + t);
+            return data;
+        }
+    }
+
+    /**
+     * Gets a Stream of objects from a bucket
+     * @param bucketName name of the bucket
+     * @param directoryPrefix prefix we use to get files in the directory
+     * @return a Stream of Blob of type I
+     */
+    Stream<I> stream(String bucketName, String directoryPrefix);
+
+    /**
+     * Gets a Stream of String from a bucket
+     *
+     * @param bucketName name of the bucket
+     * @param directoryPrefix prefix we use to get files in the directory
+     * @return a Stream of String after reading each Blob as String use UTF-8 encoding
+     */
+    default Stream<String> allContent(String bucketName, String directoryPrefix) {
+        return stream(bucketName,directoryPrefix).map( this::utf8 );
+    }
+
+    /**
+     * Gets a Stream of Object from a bucket
+     * after reading each Blob as String use UTF-8 encoding
+     * In case it can parse it as JSON return that object, else return the string
+     * @param bucketName name of the bucket
+     * @param directoryPrefix prefix we use to get files in the directory
+     * @return a Stream of Object or String
+     */
+    default Stream<Object> allData(String bucketName, String directoryPrefix) {
+        return stream(bucketName, directoryPrefix).map( this::json );
+    }
+
+    /**
+     * Create a new bucket
+     *
+     * @param bucketName name of the bucket
+     * @return a type B
+     */
+    B createBucket(String bucketName);
+
+    /**
+     * Deletes the bucket
+     * @param bucketName name of bucket
+     * @return true if bucket was deleted false if bucket does not exist
+     */
+    boolean deleteBucket(String bucketName);
+
+    /**
+     * Deletes the file from the bucket
+     * @param bucketName name of the bucket
+     * @param path path of the file - example - "abc/def.json"
+     * @return true if file was deleted, false if file does not exist
+     */
+    boolean delete(String bucketName, String path);
+
+}
--- app/src/main/java/cowj/plugins/S3StorageWrapper.java
+++ app/src/main/java/cowj/plugins/S3StorageWrapper.java
@@ -1,5 +1,6 @@
 package cowj.plugins;
 
+import cowj.StorageWrapper;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -12,30 +13,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
-import zoomba.lang.core.types.ZTypes;
 
 import java.util.stream.Stream;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 /**
  * Abstraction for AWS S3 Cloud Storage
  */
-public interface S3StorageWrapper {
+public interface S3StorageWrapper extends StorageWrapper<Boolean, PutObjectResponse, ResponseBytes<GetObjectResponse>> {
 
     /**
      * Logger for the wrapper
      */
     Logger logger = LoggerFactory.getLogger(S3StorageWrapper.class);
-
-    static boolean safeBoolean( Runnable runnable ){
-        try {
-            runnable.run();
-            return true;
-        }catch (Throwable th){
-            logger.warn("Will return false - Error happened " + th);
-            return false;
-        }
-    }
 
     /**
      * Underlying S3Client instance
@@ -45,31 +34,28 @@ public interface S3StorageWrapper {
     S3Client s3client();
 
     /**
-     * Dump String to Google Cloud Storage
+     * Dump String to S3 Cloud Storage
      *
      * @param bucketName the bucket
      * @param fileName   the file
      * @param data       which to be dumped encoding used is UTF-8
      * @return a PutObjectResponse
      */
+    @Override
     default PutObjectResponse dumps(String bucketName, String fileName, String data) {
         return s3client().putObject(PutObjectRequest.builder().bucket(bucketName).key(fileName).build(),
                 RequestBody.fromString(data));
     }
 
-    /**
-     * Dump Object to Google Cloud Storage after converting it to JSON String
-     *
-     * @param bucketName the bucket
-     * @param fileName   the file
-     * @param obj        which to be dumped
-     * @return a Blob object
-     */
-    default PutObjectResponse dump(String bucketName, String fileName, Object obj) {
-        String objJsonString = ZTypes.jsonString(obj);
-        return dumps(bucketName, fileName, objJsonString);
+    @Override
+    default byte[] bytes(ResponseBytes<GetObjectResponse> input){
+        return input.asByteArray();
     }
 
+    @Override
+    default String utf8(ResponseBytes<GetObjectResponse> input){
+        return input.asUtf8String();
+    }
 
     /**
      * In case file exists
@@ -77,6 +63,7 @@ public interface S3StorageWrapper {
      * @param fileName having the name
      * @return true if it is a blob , false if it does not exist
      */
+    @Override
     default boolean fileExist(String bucketName, String fileName) {
        return safeBoolean( () -> {
            HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
@@ -88,37 +75,15 @@ public interface S3StorageWrapper {
        });
     }
 
-    /**
-     * Load data from Google Storage as String - encoding is UTF-8
-     *
-     * @param bucketName from this bucket name
-     * @param fileName   from this file
-     * @return data string - content of the file
-     */
-    default String loads(String bucketName, String fileName) {
-        byte[] blob = loadb(bucketName, fileName);
-        if (blob == null) {
-            return "";
-        }
-        return new String(blob, UTF_8);
-    }
-
-    /**
-     * Load data from Google Storage as bytes
-     *
-     * @param bucketName from this bucket name
-     * @param fileName   from this file
-     * @return byte[] - content of the file
-     */
-    default byte[] loadb(String bucketName, String fileName) {
+    @Override
+    default ResponseBytes<GetObjectResponse> data(String bucketName, String fileName){
         try {
             GetObjectRequest objectRequest = GetObjectRequest
                     .builder()
                     .key(fileName)
                     .bucket(bucketName)
                     .build();
-            ResponseBytes<GetObjectResponse> objectBytes = s3client().getObjectAsBytes(objectRequest);
-            return objectBytes.asByteArray();
+            return s3client().getObjectAsBytes(objectRequest);
         }catch (Throwable th){
             logger.warn("Error loading data : " + th);
             return null;
@@ -126,31 +91,13 @@ public interface S3StorageWrapper {
     }
 
     /**
-     * Load data from Google Storage as Object
-     *
-     * @param bucketName from this bucket name
-     * @param fileName   from this file
-     * @return data object - content of the file after parsing it as JSON
-     */
-    default Object load(String bucketName, String fileName) {
-        String data = loads(bucketName, fileName);
-        if (data.isEmpty()) return null;
-        try {
-            return ZTypes.json(data);
-        } catch (Throwable t) {
-            logger.warn("Error jsonifying object data - will return raw data: " + t);
-            return data;
-        }
-    }
-
-    /**
      * Gets a Stream of Blob objects from a bucket
-     * <a href="https://cloud.google.com/storage/docs/samples/storage-list-files-with-prefix#storage_list_files_with_prefix-java">See In Here.</a>
      * @param bucketName name of the bucket
      * @param directoryPrefix prefix we use to get files in the directory
      * @return a Stream of Google Storage Blob
      */
-    default Stream<S3Object> stream(String bucketName, String directoryPrefix) {
+    @Override
+    default Stream<ResponseBytes<GetObjectResponse>> stream(String bucketName, String directoryPrefix) {
         ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
                 .bucket(bucketName )
                 .maxKeys(pageSize()) // Set the maxKeys parameter to control the page size
@@ -158,9 +105,10 @@ public interface S3StorageWrapper {
                 .build();
 
         ListObjectsV2Iterable listObjectsV2Iterable = s3client().listObjectsV2Paginator(listObjectsV2Request);
-        Stream<S3Object> resultStream = Stream.of();
+        Stream<ResponseBytes<GetObjectResponse>> resultStream = Stream.of();
         for (ListObjectsV2Response page : listObjectsV2Iterable) {
-            final Stream<S3Object> stream = page.contents().stream();
+            final Stream<ResponseBytes<GetObjectResponse>> stream =
+                    page.contents().stream().map( so -> data(bucketName, so.key() ));
             resultStream = Stream.concat(resultStream, stream );
         }
         return resultStream;
@@ -168,28 +116,6 @@ public interface S3StorageWrapper {
 
     default int pageSize(){ return  1000 ; }
 
-    /**
-     * Gets a Stream of String from a bucket
-     *
-     * @param bucketName name of the bucket
-     * @param directoryPrefix prefix we use to get files in the directory
-     * @return a Stream of String after reading each Blob as String use UTF-8 encoding
-     */
-    default Stream<String> allContent(String bucketName, String directoryPrefix) {
-        return stream(bucketName,directoryPrefix).map(so -> loads(bucketName, so.key()));
-    }
-
-    /**
-     * Gets a Stream of Object from a bucket
-     * after reading each Blob as String use UTF-8 encoding
-     * In case it can parse it as JSON return that object, else return the string
-     * @param bucketName name of the bucket
-     * @param directoryPrefix prefix we use to get files in the directory
-     * @return a Stream of Object or String
-     */
-    default Stream<Object> allData(String bucketName, String directoryPrefix) {
-        return stream(bucketName, directoryPrefix).map( so -> load(bucketName, so.key()));
-    }
 
     /**
      * Create a new bucket
@@ -197,7 +123,8 @@ public interface S3StorageWrapper {
      * @param bucketName name of the bucket
      * @return true if it was created, false otherwise
      */
-    default boolean createBucket(String bucketName) {
+    @Override
+    default Boolean createBucket(String bucketName) {
         final S3Client s3Client = s3client();
         return safeBoolean( () -> {
             s3Client.createBucket(CreateBucketRequest
@@ -217,6 +144,7 @@ public interface S3StorageWrapper {
      * @param bucketName name of bucket
      * @return true if bucket was deleted false if bucket does not exist
      */
+    @Override
     default boolean deleteBucket(String bucketName) {
         return safeBoolean( () -> s3client().deleteBucket( DeleteBucketRequest.builder().bucket(bucketName).build()));
     }
@@ -227,6 +155,7 @@ public interface S3StorageWrapper {
      * @param path path of the file - example - "abc/def.json"
      * @return true if file was deleted, false if file does not exist
      */
+    @Override
     default boolean delete(String bucketName, String path) {
         return safeBoolean( () ->
                 s3client().deleteObject( DeleteObjectRequest.builder().bucket(bucketName).key(path).build()));
