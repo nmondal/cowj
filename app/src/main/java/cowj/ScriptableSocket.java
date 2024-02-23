851a2c88dd896f07af7bc03cf37f3f6eb1372681
noga <nabarun.mondal@gmail.com>
2024-02-23 12:39:14 +0530

    Adding javadoc to the mix.

--- app/src/main/java/cowj/ScriptableSocket.java
+++ app/src/main/java/cowj/ScriptableSocket.java
@@ -10,16 +10,42 @@ import javax.script.SimpleBindings;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
-
+/**
+ * Template jetty based WebSocket implementation
+ * This wraps around a Scriptable
+ */
 @WebSocket
 public final class ScriptableSocket {
 
+    /**
+     * A Payload for Any WebSocket event
+     */
     static class SocketEvent{
 
+        /**
+         * Type of the event, one of
+         * connect, closed, message, error, frame
+         * As verbatim
+         */
         public final String type;
+
+        /**
+         * A jetty WebSocket Session
+         */
         public final Session session;
 
+        /**
+         * Either null, String, Frame, Throwable
+         * Based on the event type
+         * closed, message : String
+         * frame : Frame
+         * error : Throwable
+         */
         public final Object data;
+
+        /**
+         * Code , always -1, except in close, when it depicts the code for closure
+         */
         public final int code;
 
         private SocketEvent(String type, Session session, Object data, int code){
@@ -36,10 +62,16 @@ public final class ScriptableSocket {
      */
     Logger logger = LoggerFactory.getLogger(ScriptableSocket.class);
 
+    /**
+     * A holder for all sessions across all WebSocket connections across paths
+     */
     public static final Map<String, Set<Session>> SESSIONS = new ConcurrentHashMap<>();
 
     private final Scriptable scriptable;
 
+    /**
+     * Which path mapping this handler has
+     */
     public final String path;
 
     private ScriptableSocket(String wsPath, String handler){
@@ -48,15 +80,44 @@ public final class ScriptableSocket {
         scriptable = Scriptable.UNIVERSAL.create( "ws://" + wsPath, handler);
     }
 
+    /**
+     * Creates in instance from path and handler path
+     * @param wsPath web socket path route
+     * @param handler Scriptable handler path
+     * @return an instance of ScriptableSocket to handle the websocket
+     */
     public static ScriptableSocket socket(String wsPath, String handler){
         return new ScriptableSocket(wsPath, handler);
     }
 
+    /**
+     * Event Variable Key for the Scriptable
+     */
     public static final String EVENT = "event" ;
+
+    /**
+     * Connect Event
+     */
     public static final String EVENT_CONNECT = "connect" ;
+
+    /**
+     * Closed Event
+     */
     public static final String EVENT_CLOSED = "closed" ;
+
+    /**
+     * Message Event
+     */
     public static final String EVENT_MESSAGE = "message" ;
+
+    /**
+     * Error Event
+     */
     public static final String EVENT_ERROR = "error" ;
+
+    /**
+     * Frame Event
+     */
     public static final String EVENT_FRAME = "frame" ;
 
     private void handleEvent( SocketEvent event){
@@ -69,12 +130,23 @@ public final class ScriptableSocket {
         }
     }
 
+    /**
+     * Template method for passing 'connected' event to Scriptable
+     * @param session jetty WebSocket Session
+     */
     @OnWebSocketConnect
     public void connected(Session session) {
         logger.debug("connect - {}", session);
         SESSIONS.get(path).add(session);
         handleEvent( new SocketEvent(EVENT_CONNECT, session, null, -1));
     }
+
+    /**
+     * Template method for passing 'closed' event to Scriptable
+     * @param session jetty WebSocket Session
+     * @param statusCode code for why connection was closed
+     * @param reason String - human description for why connection was closed
+     */
 
     @OnWebSocketClose
     public void closed(Session session, int statusCode, String reason) {
@@ -83,18 +155,33 @@ public final class ScriptableSocket {
         SESSIONS.get(path).remove(session);
     }
 
+    /**
+     * Template method for passing 'frame' event to Scriptable
+     * @param session jetty WebSocket Session
+     * @param frame a jetty Frame
+     */
     @OnWebSocketFrame
     public void frame(Session session, Frame frame){
         logger.debug("frame - {} , frame {}", session, frame);
         handleEvent( new SocketEvent(EVENT_FRAME, session, frame, -1));
     }
 
+    /**
+     * Template method for passing 'message' event to Scriptable
+     * @param session jetty WebSocket Session
+     * @param message the String which is received from the client
+     */
     @OnWebSocketMessage
     public void message(Session session, String message)  {
         logger.debug("message - {} , message {}", session, message );
         handleEvent( new SocketEvent(EVENT_MESSAGE, session, message, -1));
     }
 
+    /**
+     * Template method for passing 'error' event to Scriptable
+     * @param session jetty WebSocket Session
+     * @param error Throwable that was encountered
+     */
     @OnWebSocketError
     public void error(Session session, Throwable error){
         logger.debug("error - {} , message {}", session, error.toString() );
