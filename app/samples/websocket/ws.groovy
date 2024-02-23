1e4c7ea7ae36c30262346ca01d089b4afd7de236
noga <nabarun.mondal@gmail.com>
2024-02-23 11:39:17 +0530

    Things run, generally.

--- app/samples/websocket/ws.groovy
+++ app/samples/websocket/ws.groovy
@@ -1,1 +1,6 @@
 _log.info(event.type)
+if ( event.type == "connect"){
+    event.session.getRemote().sendString("Welcome!");
+} else if ( event.type == "message") {
+    event.session.getRemote().sendString("ya!");
+}
