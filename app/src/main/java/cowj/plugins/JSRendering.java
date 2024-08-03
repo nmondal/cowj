650dc85740b74e3ffa7eca58388628f31af4e2b2
nmondal <nabarun.mondal@gmail.com>
2024-08-03 13:44:33 +0530

    Better refactoring.

--- app/src/main/java/cowj/plugins/JSRendering.java
+++ app/src/main/java/cowj/plugins/JSRendering.java
@@ -4,6 +4,8 @@ import cowj.DataSource;
 import cowj.EitherMonad;
 import cowj.Model;
 import cowj.Scriptable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.script.ScriptEngine;
 import java.io.InputStream;
@@ -15,11 +17,30 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is the Plugin for JavaScript Server Side Rendering
+ * It loads a bunch of contexts, and then you can simply render any page via render() call
+ */
 public interface JSRendering {
 
+    /**
+     * Logger for the wrapper
+     */
+    Logger logger = LoggerFactory.getLogger(JSRendering.class);
+
+    /**
+     * Renders to String from data accumulated from the script file using the template file
+     * @param scriptPath JavaScript file that would be run to produce the data ( actual SSR )
+     * @param htmlTemplatePath template file that would be returned after populating the data
+     * @return some HTML rendered on Server Side
+     */
     default EitherMonad<String> render(String scriptPath, String htmlTemplatePath){
         EitherMonad<Object> em = evalScript( engine(), scriptPath, model().staticPath() );
-        if ( em.inError() ) return EitherMonad.error( new IllegalStateException("Error in Script : " + scriptPath, em.error()));
+        if ( em.inError() ) {
+            final String message = "Error in Script :" + scriptPath ;
+            logger.error(message, em.error());
+            return EitherMonad.error( em.error() );
+        }
         if ( em.value() instanceof Map<?,?> ){
             return EitherMonad.call( () ->{
                 final Map<?,?> context = (Map<?,?>)em.value();
@@ -30,8 +51,16 @@ public interface JSRendering {
         return EitherMonad.error(new IllegalStateException("Renderer Function did not return a Map!"));
     }
 
+    /**
+     * Underlying Scripting Engine
+     * @return A JSR223 JavaScript Engine
+     */
     ScriptEngine engine();
 
+    /**
+     * Underlying Model
+     * @return a Model
+     */
     Model model();
 
     private static String relocAndLoadFileData( String path, String modelStaticPath) throws Exception {
@@ -41,14 +70,23 @@ public interface JSRendering {
         } else {
             localPath =  modelStaticPath + "/" + path ;
         }
+        logger.info("Reading script locally : {}", localPath);
         return  Files.readString(Paths.get(localPath));
     }
 
+    /**
+     * Evaluates the script using the engine
+     * @param engine JSR223 JavaScript Engine
+     * @param scriptPath path of the script ( either absolute, or relative to the modelStaticPath )
+     * @param modelStaticPath the static path of the model
+     * @return result of the script execution
+     */
     static EitherMonad<Object> evalScript(ScriptEngine engine, String scriptPath, String modelStaticPath){
         return EitherMonad.call( () -> {
             final String scriptBody;
             if (scriptPath.startsWith("http")) {
                 URL u = new URL(scriptPath);
+                logger.info("Reading script url : {}", scriptPath);
                 try (InputStream in = u.openStream()) {
                     scriptBody = new String(in.readAllBytes(), StandardCharsets.UTF_8);
                 }
@@ -59,6 +97,9 @@ public interface JSRendering {
         });
     }
 
+    /**
+     * Key for the context scripts
+     */
     String CONTEXT = "context" ;
 
 
@@ -71,7 +112,11 @@ public interface JSRendering {
             List<String> scripts = (List<String>)config.getOrDefault( CONTEXT , Collections.emptyList());
             for ( String path : scripts ){
                EitherMonad<Object> em = evalScript(engine, path, parent.staticPath() );
-               if ( em.inError() ) throw new IllegalStateException( "Error in Script : " + path, em.error() );
+               if ( em.inError() ){
+                   final String message = "Error in Script :" + path ;
+                   logger.error(message, em.error());
+                   throw em.error();
+               }
             }
 
             final JSRendering renderer = new JSRendering() {
