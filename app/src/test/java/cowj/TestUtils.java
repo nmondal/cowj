3bd75f33c7d2dc809eb87ca23325a727643b5893
noga <nabarun.mondal@gmail.com>
2024-09-18 21:28:59 +0530

    Fixed perf test in prefixLogger.

--- app/src/test/java/cowj/TestUtils.java
+++ app/src/test/java/cowj/TestUtils.java
@@ -3,10 +3,10 @@ package cowj;
 public class TestUtils {
 
     public static  EitherMonad<Long> timeIt( Runnable r){
-        long start = System.nanoTime();
+        final long start = System.nanoTime();
         try{
             r.run();
-            long end = System.nanoTime();
+            final long end = System.nanoTime();
             return EitherMonad.value(end - start);
         }catch (Throwable err){
             return EitherMonad.error(err);
