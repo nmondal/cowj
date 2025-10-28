2afb4bd98618e3b87c972ac6dcd7f94cc9fde5a1
noga <nabarun.mondal@gmail.com>
2025-10-28 15:42:17 +0530

    Updated with latest version of 23.xxx graal.

--- app/graal.sh
+++ app/graal.sh
@@ -3,7 +3,7 @@
 #  Lots of parameters needs to be passed, so to remember here is the script
 #  Build job creates the graal compiler directory at libs/graal
 #  Dated Oct 28 2025
-#  There is a bug that prevents it from upgrading to latest graal js on JVM 21
+#  There is a bug that prevents it from upgrading to latest graal js on JVM 21 -- 23 is the limit for Graal binaries
 #  https://github.com/oracle/graal/issues/7651
 ############################################
 
